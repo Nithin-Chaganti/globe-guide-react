5dc5df0a965fac832694749e65cd943a8591a1e1
Chaganti Nithin <chgagantinithin598@gmail.com>
2026-05-31 12:47:27 +0530

    Add hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,13 @@
+import { HeroSection } from "../components/ui/HeroSection";
+import About from "./About";
+
 const Home = () => {
   return (
-    <section className="hero-section">
-      <div className="container grid grid-two-cols">
-        <div className="hero-content">
-          <h1 className="heading-xl">Home</h1>
-          <p className="paragraph">
-            This is the home page of the multi page React app.
-          </p>
-        </div>
+    <>
+      <HeroSection />
+      <About />
+    </>
+  );
+};
 
-       
-      </div>
-    </section>
-  )
-}
-
-export default Home
+export default Home;
